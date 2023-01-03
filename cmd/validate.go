488785c45b811db683eb1decb4109041c5ff827a
Tristan Cartledge <tristan@speakeasyapi.dev>
2023-01-03 18:13:28 +0000

    fix: support versioned homebrew releases

--- cmd/validate.go
+++ cmd/validate.go
@@ -7,15 +7,15 @@ import (
 
 var validateCmd = &cobra.Command{
 	Use:   "validate",
-	Short: "Validate OpenAPI schemas + more (coming soon)",
-	Long:  `The "validate" command provides a set of commands for validating OpenAPI specs and more (coming soon).`,
+	Short: "Validate OpenAPI documents + more (coming soon)",
+	Long:  `The "validate" command provides a set of commands for validating OpenAPI docs and more (coming soon).`,
 	RunE:  validateExec,
 }
 
 var validateOpenAPICmd = &cobra.Command{
 	Use:   "openapi",
-	Short: "Validate an OpenAPI schema",
-	Long:  `Validates an OpenAPI schema is valid and conforms to the Speakeasy OpenAPI specification.`,
+	Short: "Validate an OpenAPI document",
+	Long:  `Validates an OpenAPI document is valid and conforms to the Speakeasy OpenAPI specification.`,
 }
 
 func validateInit() {
@@ -25,7 +25,7 @@ func validateInit() {
 
 //nolint:errcheck
 func validateOpenInit() {
-	validateOpenAPICmd.Flags().StringP("schema", "s", "", "path to the openapi schema")
+	validateOpenAPICmd.Flags().StringP("schema", "s", "", "path to the OpenAPI document")
 	validateOpenAPICmd.MarkFlagRequired("schema")
 
 	validateOpenAPICmd.RunE = validateOpenAPI
