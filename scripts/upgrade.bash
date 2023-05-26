e1f59189b44005d9dce9aef38db2223a6a7504e0
Thomas Rooney <thomas@resiliencycollective.com>
2023-05-26 16:19:05 +0100

    feat: php formatting, terraform body logs, terraform fixes (#104)

--- scripts/upgrade.bash
+++ scripts/upgrade.bash
@@ -7,37 +7,56 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
   cd "${SCRIPT_DIR}/.."
   export GOPRIVATE=github.com/speakeasy-api/*
   read -r CURRENT_VERSION <<<$(git describe --tags | awk '{print substr($1,2); }')
-  echo "  => CURRENT_VERSION=${CURRENT_VERSION}"
+#  echo "  => CURRENT_VERSION=${CURRENT_VERSION}"
 
   read -r CURRENT_OPENAPI_GENERATION_VERSION <<<$(cat go.mod | grep github.com/speakeasy-api/openapi-generation/v2 | awk '{ print $2 }' | awk '{print substr($1,2); }')
-  echo "  => CURRENT_OPENAPI_GENERATION_VERSION=${CURRENT_OPENAPI_GENERATION_VERSION}"
+#  echo "  => CURRENT_OPENAPI_GENERATION_VERSION=${CURRENT_OPENAPI_GENERATION_VERSION}"
 
-  read -r NEXT_OPENAPI_GENERATION_VERSION <<<$(go list -m -versions github.com/speakeasy-api/openapi-generation/v2 | awk '{print $(NF)}' | awk '{print substr($1,2); }')
-  echo "  => NEXT_OPENAPI_GENERATION_VERSION=${NEXT_OPENAPI_GENERATION_VERSION}"
+  read -r ALL_OPENAPI_GENERATION_VERSION <<<$(go list -m -versions -json github.com/speakeasy-api/openapi-generation/v2 | jq -r '.Versions | .[]' | awk '{print substr($1,2); }')
+#  echo "  => ALL_OPENAPI_GENERATION_VERSION=${ALL_OPENAPI_GENERATION_VERSION}"
+  LAST=
+  START_DATE=
+  for VERSION in $ALL_OPENAPI_GENERATION_VERSION; do
+    read -r IS_BIGGER <<<$("${SCRIPT_DIR}/semver.bash" compare "${VERSION}" "${CURRENT_OPENAPI_GENERATION_VERSION}")
+    if [[ $IS_BIGGER == "1" && -z $START_DATE ]]; then
+      START_DATE=$(gh search commits "v$LAST" --repo speakeasy-api/openapi-generation --json "commit" | jq -r '.[] | .commit.committer.date')
+#      echo "  => START_DATE=${START_DATE}"
+      LAST=$VERSION
+    fi
+    LAST=$VERSION
+  done
+
+  if [[ -z $START_DATE ]]; then
+    echo "Could not find any recent commits"
+    exit 1
+  fi
+
+  ALL_COMMIT_MESSAGES=$(gh search commits " " --committer-date=">$START_DATE" --repo speakeasy-api/openapi-generation --json "commit" | jq '.[] | .commit.message')
+#  echo "  => ALL_COMMIT_MESSAGES=${ALL_COMMIT_MESSAGES}"
+  NEXT_OPENAPI_GENERATION_VERSION=$LAST
+#  echo "  => NEXT_OPENAPI_GENERATION_VERSION=${NEXT_OPENAPI_GENERATION_VERSION}"
 
   read -r SEMVER_CHANGE <<<$("${SCRIPT_DIR}/semver.bash" diff "${CURRENT_OPENAPI_GENERATION_VERSION}" "${NEXT_OPENAPI_GENERATION_VERSION}")
-  echo "  => SEMVER_CHANGE=${SEMVER_CHANGE}"
+#  echo "  => SEMVER_CHANGE=${SEMVER_CHANGE}"
 
-  if [[ "${SEMVER_CHANGE}" == "major" ]]; then
-    echo "  => MAJOR SEMVER_CHANGE detected in downstream library. Downgrading it to minor as we're wrapped in a CLI"
-    SEMVER_CHANGE=minor
+  if [[ "$SEMVER_CHANGE" == "none" || -z $SEMVER_CHANGE ]]; then
+    echo "  => No SEMVER_CHANGE detected in downstream library. Exiting"
+    exit 0
   fi
 
   read -r BUMPED_CURRENT_VERSION <<<$("${SCRIPT_DIR}/semver.bash" bump "${SEMVER_CHANGE}" "${CURRENT_VERSION}")
-  echo "  => BUMPED_CURRENT_VERSION=${BUMPED_CURRENT_VERSION}"
+#  echo "  => BUMPED_CURRENT_VERSION=${BUMPED_CURRENT_VERSION}"
+  echo "  ===== Commit Messages ==== "
+  echo "$ALL_COMMIT_MESSAGES"
+  echo "  ===== End Commit Messages ==== "
+  read -p "Commit Message (please summarize changes above): " SUMMARY
 
-  read -p "About to bump $SEMVER_CHANGE from $CURRENT_VERSION to $BUMPED_CURRENT_VERSION. Confirm (y/N): " choice
-  case "$choice" in
-    y );;
-    * ) exit 1;;
-  esac
+  go get -v "github.com/speakeasy-api/openapi-generation/v2@v${NEXT_OPENAPI_GENERATION_VERSION}"
 
   echo "$ git add -A"
   git add -A
-  echo "$ git commit -m"chore: bump openapi-generation/v2 to ${NEXT_OPENAPI_GENERATION_VERSION}""
-  git commit -m"chore: bump openapi-generation/v2 to ${NEXT_OPENAPI_GENERATION_VERSION}"
-  echo "$ git tag \"v${BUMPED_CURRENT_VERSION}\""
-  git tag "v${BUMPED_CURRENT_VERSION}"
+  echo "$ git commit -m\"$SUMMARY\""
+  git commit -m"$SUMMARY"
   echo "===== When you are ready, execute the following command to upgrade ====="
-  echo "$ git push origin main --tags"
+  echo "$ git push origin main"
 )
