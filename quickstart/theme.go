a73fa9c9d372888598adefe0273be77fc6938585
Ryan Albert <ryantimalbert@gmail.com>
2024-01-08 18:40:29 -0800

    feat: save progress

--- quickstart/theme.go
+++ quickstart/theme.go
@@ -9,7 +9,7 @@ import (
 var theme *huh.Theme
 
 func init() {
-	theme = huh.ThemeCharm()
+	theme = huh.ThemeBase()
 	theme.Form = theme.Form.Copy()
 	theme.FieldSeparator = theme.FieldSeparator.Copy()
 	theme.FieldSeparator.Foreground(lipgloss.Color("#FBE331"))
@@ -19,28 +19,21 @@ func init() {
 	theme.Form.Background(lipgloss.Color("#FBE331"))
 	theme.Group.Foreground(lipgloss.Color("#FBE331"))
 	theme.Group.Background(lipgloss.Color("#FBE331"))
+
 	f := &theme.Focused
-	b := &theme.Blurred
-	f.Title.Foreground(lipgloss.AdaptiveColor{Light: "#FBE331", Dark: "#FBE331"}).Bold(true).Background(lipgloss.NoColor{})
-	b.Title.Foreground(lipgloss.AdaptiveColor{Light: "#AF9A04", Dark: "#AF9A04"}).Bold(true).Background(lipgloss.NoColor{})
 
-	f.Base = f.Base.Foreground(lipgloss.Color("#FBE331"))
-	b.Base = f.Base.Foreground(lipgloss.Color("#AF9A04"))
+	f.Title.Foreground(lipgloss.Color("#FBE331")).Bold(true)
+	f.Title.Background(lipgloss.Color("#FBE331")).Bold(true)
+	f.Description.Foreground(lipgloss.Color("#FBE331")).Bold(true)
+	f.Description.Background(lipgloss.Color("#FBE331")).Bold(true)
+	f.Base.Foreground(lipgloss.Color("#FBE331"))
+	f.Option.Background(lipgloss.Color("#FBE331"))
+	f.Option.Foreground(lipgloss.Color("#FBE331"))
 	f.SelectSelector.Foreground(lipgloss.Color("#FBE331"))
-	f.SelectedOption.Foreground(lipgloss.Color("#FBE331"))
-	f.MultiSelectSelector.Foreground(lipgloss.Color("#FBE331"))
-	f.SelectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#FBE331", Dark: "#FBE331"}).SetString("✓ ")
-	f.UnselectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#FBE331", Dark: "#FBE331"}).SetString("• ")
-	f.FocusedButton.Background(lipgloss.Color("#FBE331"))
-	f.Next = f.FocusedButton.Copy()
+	f.SelectSelector = lipgloss.NewStyle().
+		SetString("* ")
+	f.SelectSelector.Background(lipgloss.Color("#FBE331"))
+	f.FocusedButton.Background(lipgloss.Color("#63AC67"))
 	f.TextInput.Prompt.Foreground(lipgloss.Color("#FBE331"))
-
-	b.SelectSelector.Foreground(lipgloss.Color("#AF9A04"))
-	b.MultiSelectSelector.Foreground(lipgloss.Color("#AF9A04"))
-	b.SelectedOption.Foreground(lipgloss.Color("#AF9A04"))
-	b.SelectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#AF9A04", Dark: "#AF9A04"}).SetString("✓ ")
-	b.UnselectedPrefix = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "#AF9A04", Dark: "#AF9A04"}).SetString("• ")
-	b.FocusedButton.Background(lipgloss.Color("#AF9A04"))
-	b.Next = f.FocusedButton.Copy()
-	b.TextInput.Prompt.Foreground(lipgloss.Color("#AF9A04"))
+	f.TextInput.Cursor.Foreground(lipgloss.Color("#FBE331"))
 }
