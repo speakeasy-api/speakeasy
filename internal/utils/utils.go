aac65e5df86a562a672d50c01c7ee56f965aad4f
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2024-01-30 09:55:02 -0800

    feat: configure new target (#426)

--- internal/utils/utils.go
+++ internal/utils/utils.go
@@ -44,3 +44,25 @@ func CopyFile(src, dst string) error {
 
 	return nil
 }
+
+func MoveFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer sourceFile.Close()
+
+	destFile, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+
+	_, err = io.Copy(destFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	err = os.Remove(src)
+	return err
+}
