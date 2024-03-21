3641e509f7f10356b6b73546e83da2d85e1ba89b
Ash Godfrey <ash@speakeasyapi.dev>
2024-03-21 14:36:55 -0600

    update short/long

--- cmd/ask.go
+++ cmd/ask.go
@@ -9,15 +9,15 @@ import (
 
 var AskCmd = &model.ExecutableCommand[ask.AskFlags]{
     Usage:        "ask",
-    Short:        "Ask a question",
-    Long:         "Use this command to ask a question with the --message flag.",
+    Short:        "Ask AI",
+    Long:         "Starts a conversation with Speakeasy trained AI.",
     Run:          AskFunc,
     RequiresAuth: false, 
     Flags: []flag.Flag{
         flag.StringFlag{
             Name:        "message",
             Shorthand:   "m",
-            Description: "Your question",
+            Description: "Your question for AI.",
             Required:    false, 
         },
     },
