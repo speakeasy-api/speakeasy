fa2c07b5e6ef550cd2cb533646bf00453d368a64
Ash Godfrey <ash@speakeasyapi.dev>
2024-03-26 14:00:26 -0600

    committing changes

--- cmd/ask.go
+++ cmd/ask.go
@@ -2,26 +2,27 @@ package cmd
 
 import (
 	"context"
-	"github.com/speakeasy-api/speakeasy/internal/ask"
 	"github.com/speakeasy-api/speakeasy/internal/model"
 	"github.com/speakeasy-api/speakeasy/internal/model/flag"
+	"github.com/speakeasy-api/speakeasy/internal/ask"
 )
 
 var AskCmd = &model.ExecutableCommand[ask.AskFlags]{
-	Usage:        "ask",
-	Short:        "Ask AI",
-	Long:         "Starts a conversation with Speakeasy trained AI.",
-	Run:          AskFunc,
-	RequiresAuth: false,
-	Flags: []flag.Flag{
-		flag.StringFlag{
-			Name:        "message",
-			Shorthand:   "m",
-			Description: "Your question for AI.",
-			Required:    false,
-		},
-	},
+    Usage:        "ask",
+    Short:        "Ask AI",
+    Long:         "Starts a conversation with Speakeasy trained AI.",
+    Run:          AskFunc,
+    RequiresAuth: false, 
+    Flags: []flag.Flag{
+        flag.StringFlag{
+            Name:        "message",
+            Shorthand:   "m",
+            Description: "Your question for AI.",
+            Required:    false, 
+        },
+    },
 }
+
 
 func AskFunc(ctx context.Context, flags ask.AskFlags) error {
 	return ask.RunInteractiveChatSession(ctx, flags)
