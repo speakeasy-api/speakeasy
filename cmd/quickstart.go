461440a2c1a769a20e8984dc799332405f146006
David Alberto Adler <dalberto.adler@gmail.com>
2024-03-26 09:47:18 +0000

    Update cmd/quickstart.go

--- cmd/quickstart.go
+++ cmd/quickstart.go
@@ -71,7 +71,7 @@ func quickstartExec(ctx context.Context, flags QuickstartFlags) error {
 	}
 
 	if prompts.HasExistingGeneration(workingDir) {
-		return fmt.Errorf("You cannot run quickstart when an existing gen.yaml already exists in the direcotry. \n" +
+		return fmt.Errorf("You cannot run quickstart when an existing gen.yaml already exists in the directory. \n" +
 			"cd .. and run speakeasy quickstart to create a brand new SDK direcotry. \n" +
 			"Run speakeasy configure to add an additional SDK to this workflow. Run speakeasy run to rerun the generation workflow")
 	}
