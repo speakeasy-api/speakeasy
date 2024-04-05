7e7d7ce3a1ab3264c6519e3a71f6e65ff8cc05d7
Ryan Albert <ryantimalbert@gmail.com>
2024-04-05 08:19:37 -0700

    feat: update tests

--- integration/workflow_test.go
+++ integration/workflow_test.go
@@ -12,7 +12,180 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestCodeSampleWorkflows(t *testing.T) {
+// All tests must be run as individual test functions in serial.
+
+func TestGenerationWorkflows(t *testing.T) {
+	tests := []struct {
+		name        string
+		targetTypes []string
+		outdirs     []string
+		inputDoc    string
+	}{
+		{
+			name: "generation with remote document",
+			targetTypes: []string{
+				"go",
+			},
+			outdirs: []string{
+				"go",
+			},
+			inputDoc: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.yaml",
+		},
+		{
+			name: "generation with local document",
+			targetTypes: []string{
+				"go",
+			},
+			outdirs: []string{
+				"go",
+			},
+			inputDoc: "spec.yaml",
+		},
+		{
+			name: "multi-target generation",
+			targetTypes: []string{
+				"go",
+				"typescript",
+			},
+			outdirs: []string{
+				"go",
+				"ts",
+			},
+			inputDoc: "spec.yaml",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			temp := setupTestDir(t)
+
+			// Create workflow file and associated resources
+			workflowFile := &workflow.Workflow{
+				Version: workflow.WorkflowVersion,
+				Sources: make(map[string]workflow.Source),
+				Targets: make(map[string]workflow.Target),
+			}
+			workflowFile.Sources["first-source"] = workflow.Source{
+				Inputs: []workflow.Document{
+					{
+						Location: tt.inputDoc,
+					},
+				},
+			}
+
+			for i := range tt.targetTypes {
+				workflowFile.Targets[fmt.Sprintf("%d-target", i)] = workflow.Target{
+					Target: tt.targetTypes[i],
+					Source: "first-source",
+					Output: &tt.outdirs[i],
+				}
+			}
+
+			if isLocalFileReference(tt.inputDoc) {
+				err := copyFile("resources/spec.yaml", fmt.Sprintf("%s/%s", temp, tt.inputDoc))
+				assert.NoError(t, err)
+			}
+
+			// Execute commands from the temporary directory
+			os.Chdir(temp)
+			err := workflowFile.Validate(generate.GetSupportedLanguages())
+			assert.NoError(t, err)
+			err = os.MkdirAll(".speakeasy", 0o755)
+			assert.NoError(t, err)
+			err = workflow.Save(".", workflowFile)
+			assert.NoError(t, err)
+			args := []string{"run", "-t", "all"}
+			rootCmd.SetArgs(args)
+			cmdErr := rootCmd.Execute()
+			assert.NoError(t, cmdErr)
+
+			for i, targetType := range tt.targetTypes {
+				checkForExpectedFiles(t, tt.outdirs[i], expectedFilesByLanguage(targetType))
+			}
+		})
+	}
+}
+
+func TestSpecWorkflows(t *testing.T) {
+	tests := []struct {
+		name      string
+		inputDocs []string
+		overlays  []string
+		out       string
+	}{
+		{
+			name: "overlay with local document",
+			inputDocs: []string{
+				"spec.yaml",
+			},
+			overlays: []string{
+				"codeSamples.yaml",
+			},
+			out: "output.yaml",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			temp := setupTestDir(t)
+
+			// Create workflow file and associated resources
+			workflowFile := &workflow.Workflow{
+				Version: workflow.WorkflowVersion,
+				Sources: make(map[string]workflow.Source),
+				Targets: make(map[string]workflow.Target),
+			}
+
+			var inputs []workflow.Document
+			for _, inputDoc := range tt.inputDocs {
+				if isLocalFileReference(inputDoc) {
+					err := copyFile(fmt.Sprintf("resources/%s", inputDoc), fmt.Sprintf("%s/%s", temp, inputDoc))
+					assert.NoError(t, err)
+				}
+				inputs = append(inputs, workflow.Document{
+					Location: inputDoc,
+				})
+			}
+			var overlays []workflow.Document
+			for _, overlay := range tt.overlays {
+				if isLocalFileReference(overlay) {
+					err := copyFile(fmt.Sprintf("resources/%s", overlay), fmt.Sprintf("%s/%s", temp, overlay))
+					assert.NoError(t, err)
+				}
+				overlays = append(overlays, workflow.Document{
+					Location: overlay,
+				})
+			}
+			workflowFile.Sources["first-source"] = workflow.Source{
+				Inputs:   inputs,
+				Overlays: overlays,
+				Output:   &tt.out,
+			}
+
+			// Execute commands from the temporary directory
+			os.Chdir(temp)
+			err := workflowFile.Validate(generate.GetSupportedLanguages())
+			assert.NoError(t, err)
+			err = os.MkdirAll(".speakeasy", 0o755)
+			assert.NoError(t, err)
+			err = workflow.Save(".", workflowFile)
+			assert.NoError(t, err)
+			args := []string{"run", "-s", "all"}
+			rootCmd.SetArgs(args)
+			cmdErr := rootCmd.Execute()
+			assert.NoError(t, cmdErr)
+
+			content, err := os.ReadFile(tt.out)
+			assert.NoError(t, err, "No readable file %s exists", tt.out)
+
+			if len(tt.overlays) > 0 {
+				if !strings.Contains(string(content), " x-codeSample") {
+					t.Errorf("overlay not successfully applied to output document")
+				}
+			}
+		})
+	}
+}
+
+func TestCodeSampleGenerationWorkflows(t *testing.T) {
 	tests := []struct {
 		name       string
 		targetType string
