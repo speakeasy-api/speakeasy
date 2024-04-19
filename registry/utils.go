dd0c8330a44592f8151cde9d713777455e181912
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2024-04-19 11:29:05 -0700

    feat: integrate registry bundles into diff (#642)

---
+++ registry/utils.go
@@ -0,0 +1,27 @@
+package registry
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/speakeasy-api/sdk-gen-config/workflow"
+	"github.com/speakeasy-api/speakeasy/internal/download"
+	"github.com/speakeasy-api/speakeasy/internal/log"
+)
+
+func ResolveSpeakeasyRegistryBundle(ctx context.Context, d workflow.Document, outPath string) (string, error) {
+	log.From(ctx).Infof("Downloading bundle %s... to %s\n", d.Location, outPath)
+
+	if err := os.MkdirAll(filepath.Dir(outPath), os.ModePerm); err != nil {
+		return "", err
+	}
+
+	registryBreakdown := workflow.ParseSpeakeasyRegistryReference(d.Location)
+	if registryBreakdown == nil {
+		return "", fmt.Errorf("failed to parse speakeasy registry reference %s", d.Location)
+	}
+
+	return download.DownloadRegistryOpenAPIBundle(ctx, registryBreakdown.NamespaceID, registryBreakdown.Reference, outPath)
+}
