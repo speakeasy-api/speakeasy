af439daa98fce78094fff9c395d99b0a81cc208c
David Alberto Adler <dalberto.adler@gmail.com>
2024-04-22 19:24:03 +0100

    feat: OpenAPI diff CLI events (#646)

--- registry/utils.go
+++ registry/utils.go
@@ -11,16 +11,16 @@ import (
 	"github.com/speakeasy-api/speakeasy/internal/log"
 )
 
-func ResolveSpeakeasyRegistryBundle(ctx context.Context, d workflow.Document, outPath string) (string, error) {
+func ResolveSpeakeasyRegistryBundle(ctx context.Context, d workflow.Document, outPath string) (*download.DownloadedRegistryOpenAPIBundle, error) {
 	log.From(ctx).Infof("Downloading bundle %s... to %s\n", d.Location, outPath)
 
 	if err := os.MkdirAll(filepath.Dir(outPath), os.ModePerm); err != nil {
-		return "", err
+		return nil, err
 	}
 
 	registryBreakdown := workflow.ParseSpeakeasyRegistryReference(d.Location)
 	if registryBreakdown == nil {
-		return "", fmt.Errorf("failed to parse speakeasy registry reference %s", d.Location)
+		return nil, fmt.Errorf("failed to parse speakeasy registry reference %s", d.Location)
 	}
 
 	return download.DownloadRegistryOpenAPIBundle(ctx, registryBreakdown.NamespaceID, registryBreakdown.Reference, outPath)
