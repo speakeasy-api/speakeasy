f383626e8f829b03fe1c05f22a306bdbdea02054
David Alberto Adler <dalberto.adler@gmail.com>
2024-05-03 12:29:39 +0100

    feat: CLI events error telemetry (#682)

--- internal/workflowTracking/workflowTracking.go
+++ internal/workflowTracking/workflowTracking.go
@@ -2,8 +2,9 @@ package workflowTracking
 
 import (
 	"fmt"
-	"github.com/speakeasy-api/speakeasy/internal/log"
 	"strings"
+
+	"github.com/speakeasy-api/speakeasy/internal/log"
 
 	"github.com/speakeasy-api/speakeasy/internal/charm/styles"
 )
@@ -134,6 +135,27 @@ func (w *WorkflowStep) ListenForSubsteps(c chan log.Msg) {
 		w.NewSubstep(stepName)
 	}
 	w.ListenForSubsteps(c)
+}
+
+// Example output:
+//   - success: A -> B -> C
+//   - failure: A -> B -> C
+func (w *WorkflowStep) LastStepToString() string {
+	step := w
+	var status Status = StatusSucceeded
+	var stringNames = []string{}
+
+	for {
+		stringNames = append(stringNames, step.name)
+		status = step.status
+
+		if len(step.substeps) == 0 {
+			break
+		}
+		step = step.substeps[len(step.substeps)-1]
+	}
+
+	return fmt.Sprintf("%s: %s", status, strings.Join(stringNames, " -> "))
 }
 
 func (w *WorkflowStep) toString(parentIndent, indent int) string {
