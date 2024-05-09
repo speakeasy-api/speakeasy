c9e42587f22e9bb4fdfced0ef86fb4c9cb4be1ce
David Alberto Adler <dalberto.adler@gmail.com>
2024-05-09 16:01:38 +0100

    feat: enable registry globally

--- registry/utils.go
+++ registry/utils.go
@@ -43,6 +43,6 @@ func ResolveSpeakeasyRegistryBundle(ctx context.Context, d workflow.Document, ou
 }
 
 func IsRegistryEnabled(ctx context.Context) bool {
-	hasSchemaRegistry, _ := core.HasWorkspaceFeatureFlag(ctx, shared.FeatureFlagsSchemaRegistry)
-	return hasSchemaRegistry
+	hasSkipSchemaRegistry, _ := core.HasWorkspaceFeatureFlag(ctx, shared.FeatureFlagsSchemaRegistry)
+	return !hasSkipSchemaRegistry
 }
