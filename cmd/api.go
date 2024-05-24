a963e1ab38d57b66dcd788edad5ed1f3df25fb41
Luke Hagar <lukeslakemail@gmail.com>
2024-05-24 09:04:59 -0500

    aligned render functions

--- cmd/api.go
+++ cmd/api.go
@@ -3,15 +3,16 @@ package cmd
 import (
 	"github.com/speakeasy-api/speakeasy/internal/api"
 	"github.com/speakeasy-api/speakeasy/internal/interactivity"
+	"github.com/speakeasy-api/speakeasy/internal/log"
 	"github.com/spf13/cobra"
 )
 
 var apiCmd = &cobra.Command{
 	Use:   "api",
-	Short: "Access the Speakeasy API via the CLI",
-	Long: `Provides access to the Speakeasy API via the CLI.
+	Short: log.RenderMarkdown(`Access the Speakeasy API via the CLI`),
+	Long: log.RenderMarkdown(`Provides access to the Speakeasy API via the CLI.
 To authenticate with the Speakeasy API, you must first create an API key via https://app.speakeasyapi.dev
-and then set the SPEAKEASY_API_KEY environment variable to the value of the API key.`,
+and then set the SPEAKEASY_API_KEY environment variable to the value of the API key.`),
 	RunE:   interactivity.InteractiveRunFn("Choose an API endpoint:"),
 	Hidden: true,
 }
