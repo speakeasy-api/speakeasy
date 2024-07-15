6c7375713a76b021c09e07614734a473a03d1b7b
Thomas Rooney <thomas@speakeasyapi.dev>
2024-07-15 16:44:17 +0100

    chore: never shorten links in a CI environment

--- internal/links/links.go
+++ internal/links/links.go
@@ -2,21 +2,26 @@ package links
 
 import (
 	"context"
+	"fmt"
 	"github.com/speakeasy-api/speakeasy-client-sdk-go/v3/pkg/models/operations"
 	"github.com/speakeasy-api/speakeasy-core/auth"
+	"github.com/speakeasy-api/speakeasy-core/utils"
 	"github.com/speakeasy-api/speakeasy/internal/log"
 )
 
 func Shorten(ctx context.Context, url string) string {
+	if utils.IsRunningInCI() {
+		return url
+	}
 	client, err := auth.GetSDKFromContext(ctx)
 	if err != nil {
-		log.From(ctx).Warnf("Failed to shorten link: %s", err.Error())
+		log.From(ctx).Debug(fmt.Sprintf("Failed to shorten link: %s", err.Error()))
 		return url
 	}
 
 	res, err := client.ShortURLs.Create(ctx, operations.CreateRequestBody{URL: url})
 	if err != nil || res == nil || res.ShortURL == nil {
-		log.From(ctx).Warnf("Failed to shorten link: %s", err.Error())
+		log.From(ctx).Debug(fmt.Sprintf("Failed to shorten link: %s", err.Error()))
 		return url
 	}
 
