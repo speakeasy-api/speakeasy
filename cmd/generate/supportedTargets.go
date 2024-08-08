534b227893e2121afcedfda9fbb1b3ebf9fc4558
Ryan Albert <42415738+ryan-timothy-albert@users.noreply.github.com>
2024-08-08 11:18:28 -0700

    feat: supported targets hidden command (#847)

---
+++ cmd/generate/supportedTargets.go
@@ -0,0 +1,26 @@
+package generate
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/speakeasy-api/speakeasy/internal/model"
+	"github.com/speakeasy-api/speakeasy/internal/model/flag"
+)
+
+type supportedTargetsFlags struct{}
+
+var suportedTargetsCmd = &model.ExecutableCommand[supportedTargetsFlags]{
+	Usage:  "supported-targets",
+	Short:  "Returns a list of supported generation targets.",
+	Run:    runSupportedTargets,
+	Flags:  []flag.Flag{},
+	Hidden: true,
+}
+
+func runSupportedTargets(ctx context.Context, flags supportedTargetsFlags) error {
+	// Do not change this output structure, the sdk-generation-action depends on it
+	fmt.Println(strings.Join(SDKSupportedLanguageTargets(), ","))
+	return nil
+}
