da32c704d61a575d27f96264dbc2cc53e1b953ae
Thomas Rooney <thomas@speakeasyapi.dev>
2024-08-09 16:27:21 +0100

    chore: testing

--- internal/sdkgen/sdkgen.go
+++ internal/sdkgen/sdkgen.go
@@ -110,7 +110,7 @@ func Generate(ctx context.Context, customerID, workspaceID, lang, schemaPath, he
 		opts = append(opts, generate.WithOutputTests())
 	}
 	if skipVersioning {
-		opts = append(opts, generate.WithSkipVersioning())
+		opts = append(opts, generate.WithSkipVersioning(skipVersioning))
 	}
 
 	g, err := generate.New(opts...)
