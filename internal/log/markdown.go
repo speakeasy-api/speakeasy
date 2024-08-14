dcb63e650b187e03f1051e2ad41e807782f61d95
Robert Crumbaugh <chasecrumbaugh4@gmail.com>
2024-08-14 12:18:15 -0700

    chore: remove unused code

--- internal/log/markdown.go
+++ internal/log/markdown.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/speakeasy-api/speakeasy/internal/charm/styles"
-	"golang.org/x/exp/maps"
 	"regexp"
 )
 
@@ -20,11 +19,6 @@ func StyleMarkdown(s string) string {
 	// Extract the first style from the string, if present
 	parentStyleRegex := regexp.MustCompile(`\x1b\[.*?m`)
 	parentStyle := parentStyleRegex.FindString(s)
-
-	stopChars := "\\n"
-	for _, pattern := range maps.Keys(patternToStyle) {
-		stopChars += pattern
-	}
 
 	for pattern, style := range patternToStyle {
 		codeRx := regexp.MustCompile(fmt.Sprintf("%s([^%s]+)%s", pattern, pattern, pattern))
