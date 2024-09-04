40d4a0f3a06d3962951e8cf3d3ebb0e30ed2f868
Luke Hagar <lukeslakemail@gmail.com>
2024-09-03 19:46:01 -0700

    Update internal/env/env.go
    Co-authored-by: Brian Flad <bflad417@gmail.com>

--- internal/env/env.go
+++ internal/env/env.go
@@ -2,6 +2,9 @@ package env
 
 import "os"
 
+// Returns true if the CLI_RUNTIME environment variable is set to "docs".
+// This environment variable is used to determine when website documentation
+// is being rendered to prevent unexpected CLI formatting characters.
 func IsDocsRuntime() bool {
 	return os.Getenv("CLI_RUNTIME") == "docs"
 }
