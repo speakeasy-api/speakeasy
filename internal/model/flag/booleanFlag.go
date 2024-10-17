9abb9730747b97cd828617473aa20aab080a480e
Adam Bull <adam@speakeasy.com>
2024-10-17 15:57:31 +0100

    feat: remove force flag

--- internal/model/flag/booleanFlag.go
+++ internal/model/flag/booleanFlag.go
@@ -10,12 +10,19 @@ type BooleanFlag struct {
 	Name, Shorthand, Description string
 	Required, Hidden             bool
 	DefaultValue                 bool
+	Deprecated                   bool
+	DeprecationMessage           string
 }
 
 func (f BooleanFlag) Init(cmd *cobra.Command) error {
 	cmd.Flags().BoolP(f.Name, f.Shorthand, f.DefaultValue, f.Description)
 	if err := setRequiredAndHidden(cmd, f.Name, f.Required, f.Hidden); err != nil {
 		return err
+	}
+	if f.Deprecated {
+		if err := setDeprecated(cmd, f.Name, f.DeprecationMessage); err != nil {
+			return err
+		}
 	}
 	return nil
 }
--- internal/model/flag/stringFlag.go
+++ internal/model/flag/stringFlag.go
@@ -10,12 +10,19 @@ type StringFlag struct {
 	Required, Hidden             bool
 	DefaultValue                 string
 	AutocompleteFileExtensions   []string
+	Deprecated                   bool
+	DeprecationMessage           string
 }
 
 func (f StringFlag) Init(cmd *cobra.Command) error {
 	cmd.Flags().StringP(f.Name, f.Shorthand, f.DefaultValue, f.Description)
 	if err := setRequiredAndHidden(cmd, f.Name, f.Required, f.Hidden); err != nil {
 		return err
+	}
+	if f.Deprecated {
+		if err := setDeprecated(cmd, f.Name, f.DeprecationMessage); err != nil {
+			return err
+		}
 	}
 	if len(f.AutocompleteFileExtensions) > 0 {
 		if err := cmd.Flags().SetAnnotation(f.Name, charm.AutoCompleteAnnotation, f.AutocompleteFileExtensions); err != nil {
