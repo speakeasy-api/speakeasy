9e3fd738f986dd1214ee4cb8a065b6aa23676ea8
Robert Crumbaugh <chasecrumbaugh4@gmail.com>
2024-11-04 14:16:38 -0800

    chore: better casing

--- cmd/overlay.go
+++ cmd/overlay.go
@@ -286,9 +286,12 @@ func formatTargetPath(target string) string {
 			continue
 		}
 
-		// Don't print too much detail
-		if i >= 3 {
-			break
+		// Don't print too much detail, except for the last part
+		if i >= 3 && i < len(parts)-1 {
+			if finalParts[len(finalParts)-1] != "..." {
+				finalParts = append(finalParts, "...")
+			}
+			continue
 		}
 
 		// Don't title case paths (e.g. /v1/pets)
