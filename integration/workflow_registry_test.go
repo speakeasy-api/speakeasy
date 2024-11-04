c4abe0812be3c3ae4b82b7e4dafca2f7d6228a29
Thomas Rooney <thomas@speakeasyapi.dev>
2024-11-04 12:00:49 -0800

    chore: fix more urls

--- integration/workflow_registry_test.go
+++ integration/workflow_registry_test.go
@@ -23,7 +23,7 @@ func TestStability(t *testing.T) {
 		Sources: map[string]workflow.Source{
 			"test-source": {
 				Inputs: []workflow.Document{
-					{Location: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.yaml"},
+					{Location: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/refs/tags/3.1.0/examples/v3.0/petstore.json"},
 				},
 			},
 		},
@@ -84,7 +84,7 @@ func TestRegistryFlow(t *testing.T) {
 		Sources: map[string]workflow.Source{
 			"test-source": {
 				Inputs: []workflow.Document{
-					{Location: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.yaml"},
+					{Location: "https://raw.githubusercontent.com/OAI/OpenAPI-Specification/refs/tags/3.1.0/examples/v3.0/petstore.json"},
 				},
 			},
 		},
