1a15f3c7f0af4b635c6a25d56ec6a26897674211
Qynn Schwaab <qynn@commonsense.dev>
2025-02-25 15:43:09 -0500

    chore: clean up studio helpers

--- internal/studio/studioHandlers.go
+++ internal/studio/studioHandlers.go
@@ -10,22 +10,15 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/samber/lo"
 	"github.com/speakeasy-api/openapi-overlay/pkg/loader"
-	sdkGenConfig "github.com/speakeasy-api/sdk-gen-config"
-	"github.com/speakeasy-api/speakeasy/internal/log"
-	"github.com/speakeasy-api/speakeasy/internal/schemas"
-
 	"github.com/speakeasy-api/openapi-overlay/pkg/overlay"
-
-	"github.com/AlekSi/pointer"
-	vErrs "github.com/speakeasy-api/openapi-generation/v2/pkg/errors"
-	"github.com/speakeasy-api/speakeasy-core/errors"
-	"github.com/speakeasy-api/speakeasy/internal/studio/modifications"
-
+	sdkGenConfig "github.com/speakeasy-api/sdk-gen-config"
 	"github.com/speakeasy-api/sdk-gen-config/workflow"
+	"github.com/speakeasy-api/speakeasy-core/errors"
 	"github.com/speakeasy-api/speakeasy-core/events"
+	"github.com/speakeasy-api/speakeasy/internal/log"
 	"github.com/speakeasy-api/speakeasy/internal/run"
+	"github.com/speakeasy-api/speakeasy/internal/studio/modifications"
 	"github.com/speakeasy-api/speakeasy/internal/studio/sdk/models/components"
 	"github.com/speakeasy-api/speakeasy/internal/suggest"
 	"github.com/speakeasy-api/speakeasy/internal/utils"
@@ -143,7 +136,7 @@ func (h *StudioHandlers) reRun(ctx context.Context, w http.ResponseWriter, r *ht
 
 	h.WorkflowRunner.OnSourceResult = func(sourceResult *run.SourceResult, step string) {
 		if sourceResult.Source == h.SourceID {
-			sourceResponse, err := convertSourceResultIntoSourceResponseData(h.SourceID, *sourceResult, h.OverlayPath)
+			sourceResponseData, err := convertSourceResultIntoSourceResponseData(h.SourceID, *sourceResult, h.OverlayPath)
 			if err != nil {
 				// TODO: How to handle this error and exit the parent function?
 				fmt.Println("error converting source result to source response:", err)
@@ -158,7 +151,7 @@ func (h *StudioHandlers) reRun(ctx context.Context, w http.ResponseWriter, r *ht
 				fmt.Println("error getting last completed run result:", err)
 				return
 			}
-			response.SourceResult = *sourceResponse
+			response.SourceResult = *sourceResponseData
 
 			responseJSON, err := json.Marshal(response)
 			if err != nil {
@@ -353,7 +346,7 @@ func (h *StudioHandlers) compareOverlay(ctx context.Context, w http.ResponseWrit
 }
 
 func (h *StudioHandlers) suggestMethodNames(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-	sourceResult, err := h.runSource()
+	sourceResult, err := runSource(h.Ctx, h.WorkflowRunner, h.SourceID)
 	if err != nil {
 		return fmt.Errorf("error running source: %w", err)
 	}
@@ -401,10 +394,6 @@ func (h *StudioHandlers) exit(ctx context.Context, w http.ResponseWriter, r *htt
 	}
 	return nil
 }
-
-// ---------------------------------
-// Helper functions
-// ---------------------------------
 
 func (h *StudioHandlers) convertLastRunResult(ctx context.Context, step string) (*components.RunResponseData, error) {
 	ret := components.RunResponseData{
@@ -494,103 +483,6 @@ func (h *StudioHandlers) sendLastRunResultToStream(ctx context.Context, w http.R
 	return nil
 }
 
-func (h *StudioHandlers) runSource() (*run.SourceResult, error) {
-	workflowRunner := h.WorkflowRunner
-	sourceID := h.SourceID
-
-	workflowRunnerPtr, err := workflowRunner.Clone(h.Ctx,
-		run.WithSkipCleanup(),
-		run.WithSkipLinting(),
-		run.WithSkipGenerateLintReport(),
-		run.WithSkipSnapshot(true),
-		run.WithSkipChangeReport(true),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("error cloning workflow runner: %w", err)
-	}
-	workflowRunner = *workflowRunnerPtr
-
-	_, sourceResult, err := workflowRunner.RunSource(h.Ctx, workflowRunner.RootStep, sourceID, "")
-	if err != nil {
-		return nil, fmt.Errorf("error running source: %w", err)
-	}
-
-	return sourceResult, nil
-}
-
-func convertSourceResultIntoSourceResponseData(sourceID string, sourceResult run.SourceResult, overlayPath string) (*components.SourceResponseData, error) {
-	var err error
-	overlayContents := ""
-	if overlayPath != "" {
-		overlayContents, err = utils.ReadFileToString(overlayPath)
-		if err != nil {
-			return nil, fmt.Errorf("error reading modifications overlay: %w", err)
-		}
-	}
-
-	outputDocumentString, err := utils.ReadFileToString(sourceResult.OutputPath)
-	if err != nil && !os.IsNotExist(err) {
-		return nil, fmt.Errorf("error reading output document: %w", err)
-	}
-
-	diagnosis := make([]components.Diagnostic, 0)
-
-	if sourceResult.LintResult != nil {
-		for _, e := range sourceResult.LintResult.AllErrors {
-			vErr := vErrs.GetValidationErr(e)
-			diagnosis = append(diagnosis, components.Diagnostic{
-				Message:  vErr.Message,
-				Severity: string(vErr.Severity),
-				Line:     pointer.ToInt64(int64(vErr.LineNumber)),
-				Type:     vErr.Rule,
-			})
-		}
-	}
-
-	for t, d := range sourceResult.Diagnosis {
-		for _, diagnostic := range d {
-			diagnosis = append(diagnosis, components.Diagnostic{
-				Message:  diagnostic.Message,
-				Type:     string(t),
-				Path:     diagnostic.SchemaPath,
-				Severity: "suggestion",
-			})
-		}
-	}
-
-	finalOverlayPath := ""
-
-	if overlayPath != "" {
-		finalOverlayPath, _ = filepath.Abs(overlayPath)
-	}
-
-	// Reformat the input spec - this is so there are minimal diffs after overlay
-	inputSpecBytes := []byte(sourceResult.InputSpec)
-	isJSON := json.Valid(inputSpecBytes)
-	inputPath := "openapi.yaml"
-	if isJSON {
-		inputPath = "openapi.json"
-	}
-	inputNode := &yaml.Node{}
-	err = yaml.Unmarshal(inputSpecBytes, inputNode)
-	if err != nil {
-		return nil, fmt.Errorf("error unmarshalling input spec: %w", err)
-	}
-	formattedInputSpec, err := schemas.RenderDocument(inputNode, inputPath, !isJSON, !isJSON)
-	if err != nil {
-		return nil, fmt.Errorf("error formatting input spec: %w", err)
-	}
-
-	return &components.SourceResponseData{
-		SourceID:    sourceID,
-		Input:       string(formattedInputSpec),
-		Overlay:     overlayContents,
-		OverlayPath: finalOverlayPath,
-		Output:      outputDocumentString,
-		Diagnosis:   diagnosis,
-	}, nil
-}
-
 func (h *StudioHandlers) upsertOverlay(overlay overlay.Overlay) error {
 	workflowConfig := h.WorkflowRunner.GetWorkflowFile()
 	source := workflowConfig.Sources[h.SourceID]
@@ -613,111 +505,4 @@ func (h *StudioHandlers) upsertOverlay(overlay overlay.Overlay) error {
 	workflowConfig.Sources[h.SourceID] = source
 
 	return workflow.Save(h.WorkflowRunner.ProjectDir, workflowConfig)
-}
-
-func convertWorkflowToComponentsWorkflow(w workflow.Workflow, workingDir string) (components.Workflow, error) {
-	// 1. Marshal to JSON
-	// 2. Unmarshal to components.Workflow
-
-	jsonBytes, err := json.Marshal(w)
-	if err != nil {
-		return components.Workflow{}, err
-	}
-
-	var c components.Workflow
-	err = json.Unmarshal(jsonBytes, &c)
-	if err != nil {
-		return components.Workflow{}, err
-	}
-
-	for key, source := range c.Sources {
-		updatedInputs := lo.Map(source.Inputs, func(input components.Document, _ int) components.Document {
-			// URL
-			if strings.HasPrefix(input.Location, "https://") || strings.HasPrefix(input.Location, "http://") {
-				return input
-			}
-			// Absolute path
-			if strings.HasPrefix(input.Location, "/") {
-				return input
-			}
-			// Registry uri
-			if strings.HasPrefix(input.Location, "registry.speakeasyapi.dev") {
-				return input
-			}
-			if workingDir == "" {
-				return input
-			}
-
-			// Produce the lexically shortest path based on the base path and the location
-			shortestPath, err := filepath.Rel(workingDir, input.Location)
-
-			if err != nil {
-				shortestPath = input.Location
-			}
-
-			return components.Document{
-				Location: shortestPath,
-			}
-		})
-
-		source.Inputs = updatedInputs
-		c.Sources[key] = source
-	}
-
-	return c, nil
-}
-
-func findWorkflowSourceIDBasedOnTarget(workflow run.Workflow, targetID string) (string, error) {
-	if workflow.Source != "" {
-		return workflow.Source, nil
-	}
-
-	if targetID == "" {
-		return "", errors.ErrBadRequest.Wrap(fmt.Errorf("no source or target provided"))
-	}
-
-	workflowFile := workflow.GetWorkflowFile()
-
-	if targetID == "all" {
-		// If we're running multiple targets that's fine as long as they all have the same source
-		source := ""
-		for _, t := range workflowFile.Targets {
-			if source == "" {
-				source = t.Source
-			} else if source != t.Source {
-				return "", errors.ErrBadRequest.Wrap(fmt.Errorf("multiple targets with different sources"))
-			}
-		}
-		return source, nil
-	}
-
-	t, ok := workflowFile.Targets[targetID]
-	if !ok {
-		return "", errors.ErrBadRequest.Wrap(fmt.Errorf("target %s not found", targetID))
-	}
-
-	return t.Source, nil
-}
-
-func isStudioModificationsOverlay(overlay workflow.Overlay) (string, error) {
-	isLocalFile := overlay.Document != nil &&
-		!strings.HasPrefix(overlay.Document.Location.Resolve(), "https://") &&
-		!strings.HasPrefix(overlay.Document.Location.Resolve(), "http://") &&
-		!strings.HasPrefix(overlay.Document.Location.Resolve(), "registry.speakeasyapi.dev")
-	if !isLocalFile {
-		return "", nil
-	}
-
-	asString, err := utils.ReadFileToString(overlay.Document.Location.Resolve())
-
-	if err != nil {
-		return "", err
-	}
-
-	looksLikeStudioModifications := strings.Contains(asString, "x-speakeasy-metadata")
-	if !looksLikeStudioModifications {
-		return "", nil
-	}
-
-	return asString, nil
 }
