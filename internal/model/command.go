fe95a30bec33d1d83ce545e91add443920e7a81b
Ian Bentley <ian@idbentley.com>
2025-09-29 17:19:50 -0400

    don't fetch new build

--- internal/model/command.go
+++ internal/model/command.go
@@ -3,10 +3,9 @@ package model
 import (
 	"context"
 	"encoding/json"
-	errs "errors"
 	"fmt"
 	"os"
-	"os/exec"
+	// "os/exec"
 	"slices"
 	"strings"
 	"time"
@@ -18,7 +17,6 @@ import (
 
 	"github.com/fatih/structs"
 	"github.com/hashicorp/go-version"
-	"github.com/sethvargo/go-githubactions"
 	"github.com/speakeasy-api/sdk-gen-config/workflow"
 	"github.com/speakeasy-api/speakeasy-client-sdk-go/v3/pkg/models/shared"
 	"github.com/speakeasy-api/speakeasy-core/events"
@@ -310,74 +308,74 @@ func runWithVersionFromWorkflowFile(cmd *cobra.Command) error {
 	}
 
 	// Get lockfile version before running the command, in case it gets overwritten
-	lockfileVersion := getSpeakeasyVersionFromLockfile()
+	// lockfileVersion := getSpeakeasyVersionFromLockfile()
 
 	// If the workflow succeeds on latest, promote that version to the default
 	shouldPromote := wf.SpeakeasyVersion == "latest"
 
-	runErr := runWithVersion(cmd, artifactArch, desiredVersion, shouldPromote)
-	if runErr != nil {
-		// If the error has been marked as non-rollbackable, return the cause
-		if errors.Is(runErr, run.ErrNoRollback) {
-			return errs.Unwrap(runErr)
-		}
+	runWithVersion(cmd, artifactArch, desiredVersion, shouldPromote)
+	// if runErr != nil {
+	// 	// If the error has been marked as non-rollbackable, return the cause
+	// 	if errors.Is(runErr, run.ErrNoRollback) {
+	// 		return errs.Unwrap(runErr)
+	// 	}
 
-		// If the command failed to run with the latest version, try to run with the version from the lock file
-		if wf.SpeakeasyVersion == "latest" {
-			msg := fmt.Sprintf("Failed to run with Speakeasy version %s: %s\n", desiredVersion, runErr.Error())
-			_ = log.SendToLogProxy(ctx, log.LogProxyLevelError, msg, nil)
-			logger.PrintStyled(styles.DimmedItalic, msg)
-			if env.IsGithubAction() {
-				githubactions.AddStepSummary("# Speakeasy Version upgrade failure\n" + msg)
-			}
+	// 	// If the command failed to run with the latest version, try to run with the version from the lock file
+	// 	if wf.SpeakeasyVersion == "latest" {
+	// 		msg := fmt.Sprintf("Failed to run with Speakeasy version %s: %s\n", desiredVersion, runErr.Error())
+	// 		_ = log.SendToLogProxy(ctx, log.LogProxyLevelError, msg, nil)
+	// 		logger.PrintStyled(styles.DimmedItalic, msg)
+	// 		if env.IsGithubAction() {
+	// 			githubactions.AddStepSummary("# Speakeasy Version upgrade failure\n" + msg)
+	// 		}
 
-			if lockfileVersion != "" && lockfileVersion != desiredVersion {
-				logger.PrintfStyled(styles.DimmedItalic, "Rerunning with previous successful version")
-				// force to run local version in dev by giving malformed "latest" version
-				return runWithVersion(cmd, artifactArch, "latest", false)
-			}
-		}
+	// 		if lockfileVersion != "" && lockfileVersion != desiredVersion {
+	// 			logger.PrintfStyled(styles.DimmedItalic, "Rerunning with previous successful version")
+	// 			// force to run local version in dev by giving malformed "latest" version
+	// 			return runWithVersion(cmd, artifactArch, "latest", false)
+	// 		}
+	// 	}
 
-		// If the command failed to run with the pinned version, fail normally
-		return runErr
-	}
+	// 	// If the command failed to run with the pinned version, fail normally
+	// 	return runErr
+	// }
 
 	return nil
 }
 
 // If promote is true, the version will be promoted to the default version (ie when running `speakeasy`)
 func runWithVersion(cmd *cobra.Command, artifactArch, desiredVersion string, shouldPromote bool) error {
-	vLocation, err := updates.InstallVersion(cmd.Context(), desiredVersion, artifactArch, 30)
-	if err != nil {
-		return ErrInstallFailed.Wrap(err)
-	}
+	// vLocation, err := updates.InstallVersion(cmd.Context(), desiredVersion, artifactArch, 30)
+	// if err != nil {
+	// 	return ErrInstallFailed.Wrap(err)
+	// }
 
-	cmdParts := utils.GetCommandParts(cmd)
-	if cmdParts[0] == "speakeasy" {
-		cmdParts = cmdParts[1:]
-	}
+	// cmdParts := utils.GetCommandParts(cmd)
+	// if cmdParts[0] == "speakeasy" {
+	// 	cmdParts = cmdParts[1:]
+	// }
 
-	// The pinned flag was introduced in 1.256.0
-	// For earlier versions, it isn't necessary because we don't try auto-upgrading
-	if ok, _ := pinningWasReleased(desiredVersion); ok {
-		cmdParts = append(cmdParts, "--pinned")
-	}
+	// // The pinned flag was introduced in 1.256.0
+	// // For earlier versions, it isn't necessary because we don't try auto-upgrading
+	// if ok, _ := pinningWasReleased(desiredVersion); ok {
+	// 	cmdParts = append(cmdParts, "--pinned")
+	// }
 
-	newCmd := exec.Command(vLocation, cmdParts...)
-	newCmd.Stdin = os.Stdin
-	newCmd.Stdout = os.Stdout
-	newCmd.Stderr = os.Stderr
+	// newCmd := exec.Command(vLocation, cmdParts...)
+	// newCmd.Stdin = os.Stdin
+	// newCmd.Stdout = os.Stdout
+	// newCmd.Stderr = os.Stderr
 
-	if err = newCmd.Run(); err != nil {
-		return fmt.Errorf("failed to run with version %s: %w", desiredVersion, err)
-	}
+	// if err = newCmd.Run(); err != nil {
+	// 	return fmt.Errorf("failed to run with version %s: %w", desiredVersion, err)
+	// }
 
 	// If the workflow succeeded, make the used version the default
-	if shouldPromote && !env.IsGithubAction() && !env.IsLocalDev() {
-		if err := promoteVersion(cmd.Context(), vLocation); err != nil {
-			return fmt.Errorf("failed to promote version: %w", err)
-		}
-	}
+	// if shouldPromote && !env.IsGithubAction() && !env.IsLocalDev() {
+	// 	if err := promoteVersion(cmd.Context(), vLocation); err != nil {
+	// 		return fmt.Errorf("failed to promote version: %w", err)
+	// 	}
+	// }
 
 	return nil
 }
