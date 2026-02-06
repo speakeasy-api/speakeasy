speakeasy-api/speakeasy

commit 102ab547a08c81761c273456e6c556691af4e47c

feat: generate publish-pypi job for PyPI trusted publishing (#1854)
Adds a `publish-pypi` job to generated workflows when PyPI trusted
publishing is enabled.

The job uses the composite action
`speakeasy-api/sdk-generation-action/publish-pypi@v15` for OIDC-based
authentication.

Depends on: https://github.com/speakeasy-api/sdk-gen-config/pull/116

go.mod
@@ -39,9 +39,9 @@ require (
 	github.com/speakeasy-api/gram v0.0.0-20260121234743-5a36906a8929
 	github.com/speakeasy-api/huh v1.1.2
 	github.com/speakeasy-api/jq v0.1.1-0.20251107233444-84d7e49e84a4
-	github.com/speakeasy-api/openapi v1.15.1
-	github.com/speakeasy-api/openapi-generation/v2 v2.809.2
-	github.com/speakeasy-api/sdk-gen-config v1.47.1-0.20260204112308-f8a62f6aa764
+	github.com/speakeasy-api/openapi v1.15.0
+	github.com/speakeasy-api/openapi-generation/v2 v2.803.3
+	github.com/speakeasy-api/sdk-gen-config v1.48.1
 	github.com/speakeasy-api/speakeasy-agent-mode-content v0.2.0
 	github.com/speakeasy-api/speakeasy-client-sdk-go/v3 v3.26.7
 	github.com/speakeasy-api/speakeasy-core v0.22.0

go.sum
@@ -546,12 +546,12 @@ github.com/speakeasy-api/jsonpath v0.6.2 h1:Mys71yd6u8kuowNCR0gCVPlVAHCmKtoGXYoA
 github.com/speakeasy-api/jsonpath v0.6.2/go.mod h1:ymb2iSkyOycmzKwbEAYPJV/yi2rSmvBCLZJcyD+VVWw=
 github.com/speakeasy-api/libopenapi v0.21.9-fixhiddencomps-fixed h1:PL/kpBY5vkBmwLjg6l7J4amgSrPf3CNWReGNdwrRqJQ=
 github.com/speakeasy-api/libopenapi v0.21.9-fixhiddencomps-fixed/go.mod h1:Gc8oQkjr2InxwumK0zOBtKN9gIlv9L2VmSVIUk2YxcU=
-github.com/speakeasy-api/openapi v1.15.1 h1:t8H/zMU9EdzpfLiHfBdmVNRBXmT/o4KBE9XKoC/zYa4=
-github.com/speakeasy-api/openapi v1.15.1/go.mod h1:aiVj+JnirrwZDtKegt0hQrj/ixl3v17EkN2YGnTuSro=
-github.com/speakeasy-api/openapi-generation/v2 v2.809.2 h1:F0N4TwondXQRTdFw53tKF04CjkIaMSx56vbnOCJxxsM=
-github.com/speakeasy-api/openapi-generation/v2 v2.809.2/go.mod h1:yAqSSEVAOM8UeYHQ5KqsnFIJlCzN3TJ2OHb57PGNw9w=
-github.com/speakeasy-api/sdk-gen-config v1.47.1-0.20260204112308-f8a62f6aa764 h1:oedp+8lhOvqzL2nzChwn1xZZMrTegch7dWo7HTpOqAg=
-github.com/speakeasy-api/sdk-gen-config v1.47.1-0.20260204112308-f8a62f6aa764/go.mod h1:kD0NPNX5yaG4j+dcCpLL0hHKQbFk6X93obp+v1XlK5E=
+github.com/speakeasy-api/openapi v1.15.0 h1:/uRvnm0KGXMVI7le2EuRvb8e+XANRuJFe+U9ZGBsJqE=
+github.com/speakeasy-api/openapi v1.15.0/go.mod h1:aiVj+JnirrwZDtKegt0hQrj/ixl3v17EkN2YGnTuSro=
+github.com/speakeasy-api/openapi-generation/v2 v2.803.3 h1:UB3e52cmbVTSL0setwaIitQPApDU8arlVoZPoSD4iKc=
+github.com/speakeasy-api/openapi-generation/v2 v2.803.3/go.mod h1:ZpSl5WSnNByHgdm6h8W5cLDX+O+ZJ5zcGX4UGDX+P8I=
+github.com/speakeasy-api/sdk-gen-config v1.48.1 h1:3KHpuaKTkeaDMFEuFt3dX4t8rCyK+Tg+aK9WdC+A4sQ=
+github.com/speakeasy-api/sdk-gen-config v1.48.1/go.mod h1:kD0NPNX5yaG4j+dcCpLL0hHKQbFk6X93obp+v1XlK5E=
 github.com/speakeasy-api/speakeasy-agent-mode-content v0.2.0 h1:xqEoL+MY3gqJkDMjBqP++fieYilgI1pOeYycHfG3EUE=
 github.com/speakeasy-api/speakeasy-agent-mode-content v0.2.0/go.mod h1:AiZRZLL+sv9uwtTHIECc1dcTgfJrXrEB5QxcAGifMkI=
 github.com/speakeasy-api/speakeasy-client-sdk-go/v3 v3.26.7 h1:SoWZkRlpFlv8qibCfXWrBZay1JeLS9uqJ+1cu+DFgXo=

prompts/github.go
@@ -532,6 +532,29 @@ func WritePublishing(wf *workflow.Workflow, genWorkflow *config.GenerateWorkflow
 
 		publishingFile.Jobs.Publish.Secrets = publishingSecrets
 
+		// Add publish-pypi job if PyPI trusted publishing is enabled
+		if target.Target == "python" && target.Publishing != nil && target.Publishing.PyPi != nil &&
+			target.Publishing.PyPi.UseTrustedPublishing != nil && *target.Publishing.PyPi.UseTrustedPublishing {
+			publishingFile.Jobs.PublishPypi = &config.PublishPyPiJob{
+				Needs:  []string{"publish"},
+				If:     "${{ needs.publish.outputs.python_regenerated == 'true' && needs.publish.outputs.publish_python == 'true' && needs.publish.outputs.use_pypi_trusted_publishing == 'true' }}",
+				RunsOn: "ubuntu-latest",
+				Steps: []config.PublishPyPiStep{
+					{
+						Uses: "actions/checkout@v4",
+					},
+					{
+						Uses: "speakeasy-api/sdk-generation-action/publish-pypi@v15",
+						With: map[string]any{
+							"python-directory":    "${{ needs.publish.outputs.python_directory }}",
+							"speakeasy_api_key":   formatGithubSecretName(defaultSpeakeasyAPIKeySecretName),
+							"github_access_token": formatGithubSecretName(defaultGithubTokenSecretName),
+						},
+					},
+				},
+			}
+		}
+
 		// Write a github publishing file.
 		var publishingWorkflowBuf bytes.Buffer
 		yamlEncoder := yaml.NewEncoder(&publishingWorkflowBuf)

commit 23fc9a3941439ed88b19d41ab7c85917d63c2012

fix: restore openapi-generation to v2.811.0
Commit 102ab547 accidentally downgraded openapi-generation from v2.809.2
to v2.803.3. This restores it to the latest version (v2.811.0) to include
all the features that were already merged to main but whose dependency
version was reverted.

go.mod
@@ -40,7 +40,7 @@ require (
 	github.com/speakeasy-api/huh v1.1.2
 	github.com/speakeasy-api/jq v0.1.1-0.20251107233444-84d7e49e84a4
 	github.com/speakeasy-api/openapi v1.15.0
-	github.com/speakeasy-api/openapi-generation/v2 v2.803.3
+	github.com/speakeasy-api/openapi-generation/v2 v2.811.0
 	github.com/speakeasy-api/sdk-gen-config v1.48.1
 	github.com/speakeasy-api/speakeasy-agent-mode-content v0.2.0
 	github.com/speakeasy-api/speakeasy-client-sdk-go/v3 v3.26.7

go.sum
@@ -548,8 +548,8 @@ github.com/speakeasy-api/libopenapi v0.21.9-fixhiddencomps-fixed h1:PL/kpBY5vkBm
 github.com/speakeasy-api/libopenapi v0.21.9-fixhiddencomps-fixed/go.mod h1:Gc8oQkjr2InxwumK0zOBtKN9gIlv9L2VmSVIUk2YxcU=
 github.com/speakeasy-api/openapi v1.15.0 h1:/uRvnm0KGXMVI7le2EuRvb8e+XANRuJFe+U9ZGBsJqE=
 github.com/speakeasy-api/openapi v1.15.0/go.mod h1:aiVj+JnirrwZDtKegt0hQrj/ixl3v17EkN2YGnTuSro=
-github.com/speakeasy-api/openapi-generation/v2 v2.803.3 h1:UB3e52cmbVTSL0setwaIitQPApDU8arlVoZPoSD4iKc=
-github.com/speakeasy-api/openapi-generation/v2 v2.803.3/go.mod h1:ZpSl5WSnNByHgdm6h8W5cLDX+O+ZJ5zcGX4UGDX+P8I=
+github.com/speakeasy-api/openapi-generation/v2 v2.811.0 h1:RIVkSjIpvrkm2T3y422gFYr7seOtdOxooEMzg3M7G4E=
+github.com/speakeasy-api/openapi-generation/v2 v2.811.0/go.mod h1:yAqSSEVAOM8UeYHQ5KqsnFIJlCzN3TJ2OHb57PGNw9w=
 github.com/speakeasy-api/sdk-gen-config v1.48.1 h1:3KHpuaKTkeaDMFEuFt3dX4t8rCyK+Tg+aK9WdC+A4sQ=
 github.com/speakeasy-api/sdk-gen-config v1.48.1/go.mod h1:kD0NPNX5yaG4j+dcCpLL0hHKQbFk6X93obp+v1XlK5E=
 github.com/speakeasy-api/speakeasy-agent-mode-content v0.2.0 h1:xqEoL+MY3gqJkDMjBqP++fieYilgI1pOeYycHfG3EUE=